284caf4f129fe949a86ab36c84c7afbf74eda036
kergoth <kergoth@48e1f902-e210-0410-a839-a0138e9d5bd5>
2007-01-23 01:50:40 +0000

    Bump TOC ver and optimization/cleanup.
    - Make the decision about which combat text to notify to at addon
      load time rather than each time we loot, by generating a self.msg
      closure.
    - Make use of the string metatable to clean things up.

--- core.lua
+++ core.lua
@@ -1,59 +1,64 @@
--- Simple addon that displays information about what was looted in Mik's
--- Scrolling Battle Text.  It keeps total counts of all the items in your
--- inventory so as to show that as well as the amount just looted.
-
-local itemidpat = "(%d+):%d+:%d+:%d+"
-local linkpat = '|c........|Hitem:'..itemidpat..'.*|r'
-local function getitemid(itemlink)
-    local _, _, itemid = string.find(itemlink or "", itemidpat)
-    return itemid
-end
-
-
 LootAlert = {}
 
-function LootAlert:ADDON_LOADED(arg1)
-    if arg1 == "LootAlert" and (MikSBT or SCT_Display or SCT) then
-        this:RegisterEvent("CHAT_MSG_LOOT")
-        this:UnregisterEvent("ADDON_LOADED")
+function LootAlert:ADDON_LOADED(name)
+    if name == 'LootAlert' then
+        if MikSBT then
+            self.msg = function(message, color)
+                MikSBT.DisplayMessage(message, MikSBT.DISPLAYTYPE_NOTIFICATION, false, color.r * 255, color.g * 255, color.b * 255)
+            end
+        elseif SCT_Display then
+            self.msg = SCT_Display_Message
+        elseif SCT and SCT.DisplayText then
+            self.msg = function(message, color)
+                SCT:DisplayMessage(message, color)
+            end
+        elseif CombatText_AddMessage then
+            self.msg = function(message, color)
+                CombatText_AddMessage(message, COMBAT_TEXT_SCROLL_FUNCTION, color.r, color.g, color.b, 'sticky', nil)
+            end
+        end
+
+        if self.msg then
+            this:RegisterEvent('CHAT_MSG_LOOT')
+            this:UnregisterEvent('ADDON_LOADED')
+        end
+    elseif name == 'Blizzard_CombatText' then
+        -- This block is only used if we loaded before Blizzard_CombatText,
+        -- and the user does not have sct or msbt.  We don't want to
+        -- OptionalDeps on it, or it'll force the LoD addon to load.
+        self.msg = function(message, color)
+            CombatText_AddMessage(message, COMBAT_TEXT_SCROLL_FUNCTION, color.r, color.g, color.b, 'sticky', nil)
+        end
+
+        this:RegisterEvent('CHAT_MSG_LOOT')
+        this:UnregisterEvent('ADDON_LOADED')
     end
 end
 
-local escaped = string.gsub(linkpat, '%%', '%%%%')
-local single = string.gsub(LOOT_ITEM_SELF, '%%s', escaped)
-local multiple = string.gsub(string.gsub(LOOT_ITEM_SELF_MULTIPLE, '%%d', '(%%d+)'),
-                             '%%s', escaped)
+local linkpat = '|c........|Hitem:(%%d+):.*|r'
+local single = LOOT_ITEM_SELF:gsub('%%s', linkpat)
+local multiple = LOOT_ITEM_SELF_MULTIPLE:gsub('%%d', '(%%d+)'):gsub('%%s', linkpat)
 
 function LootAlert:CHAT_MSG_LOOT(msg)
-    local _, _, item, count = string.find(msg, multiple)
+    local item, count = msg:match(multiple)
     if not item then
-        _, _, item = string.find(msg, single)
+        item = msg:match(single)
         count = 1
     end
 
     if item then
         local oldtotal = GetItemCount(item)
         local name, _, rarity = GetItemInfo(item)
-        local message = "[Loot " .. name .. " +" .. count .. "(" .. oldtotal + count .. ")]"
+        local message = ('[Loot %s +%d(%d)]'):format(name, count, oldtotal + count)
         local color = ITEM_QUALITY_COLORS[rarity]
 
-        if MikSBT then
-            MikSBT.DisplayMessage(message, MikSBT.DISPLAYTYPE_NOTIFICATION, false, color.r * 255, color.g * 255, color.b * 255)
-        elseif SCT_Display or (SCT and SCT.DisplayText) then
-            if SCT_Display then
-                SCT_Display_Message(message, color)
-            else
-                SCT:DisplayMessage(message, color)
-            end
-        elseif CombatText_AddMessage then
-            CombatText_AddMessage(message, COMBAT_TEXT_SCROLL_FUNCTION, color.r, color.g, color.b, "sticky", nil)
-        end
+        self.msg(message, color)
     end
 end
 
 -- Initialization
-LootAlert.frame = CreateFrame("Frame", nil, UIParent)
-LootAlert.frame:SetScript("OnEvent", function(self, event, ...)
+LootAlert.frame = CreateFrame('Frame', nil, UIParent)
+LootAlert.frame:SetScript('OnEvent', function(self, event, ...)
     LootAlert[event](LootAlert, ...)
 end)
-LootAlert.frame:RegisterEvent("ADDON_LOADED")
+LootAlert.frame:RegisterEvent('ADDON_LOADED')
