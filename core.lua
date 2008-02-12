b0ece8d03ace47661176c04b1431c80260ed8deb
Chris Larson <clarson@kergoth.com>
2008-02-12 01:53:42 -0700

    Kill a bit of unnecessary string mt usage, color the item loot string such that only the item name itself is colored by rarity, and color the money loot string.. and clean up a little.

--- core.lua
+++ core.lua
@@ -1,94 +1,86 @@
+local strmatch, strformat = string.match, string.format
 local lootmessage, moneymessage, moneyformat
 
 if GetLocale() == 'zhTW' then
-    lootmessage = '[拾取 %s +%d(%d)]'
+    lootmessage = '[拾取 %s%s +%d(%d)]'
     moneymessage = '[拾取 +%s]'
-
-    moneyformat = function(gold, silver, copper)
-        return ('%s%s%s'):format(gold and ('%s金'):format(gold) or '',
-                             silver and ('%s銀'):format(silver) or '',
-                             copper and ('%s銅'):format(copper) or '')
-    end
 else
-    lootmessage = '[Loot %s +%d(%d)]'
+    lootmessage = '[Loot %s%s|r +%d(%d)]'
     moneymessage = '[Loot +%s]'
-    moneyformat = function(gold, silver, copper)
-        return ('%s%s%s'):format(gold and ('%sg'):format(gold) or '',
-                                 silver and ('%ss'):format(silver) or '',
-                                 copper and ('%sc'):format(copper) or '')
-    end
 end
-
-local msg = function(message, color)
-                UIErrorsFrame:AddMessage(message, color.r, color.g, color.b)
-            end
 
 local LootAlert = CreateFrame('Frame', nil, UIParent)
 LootAlert:SetScript('OnEvent', function(self, event, ...)
     self[event](self, ...)
 end)
-LootAlert:RegisterEvent('ADDON_LOADED')
+LootAlert:RegisterEvent('PLAYER_LOGIN')
 
-function LootAlert:ADDON_LOADED(name)
-    if name == 'LootAlert' then
-        ChatFrame1:AddMessage("lootalert loaded")
-        if MikSBT then
-            msg = function(message, color)
-                MikSBT.DisplayMessage(message, MikSBT.DISPLAYTYPE_NOTIFICATION, false, color.r * 255, color.g * 255, color.b * 255)
-            end
-        elseif SCT_Display then
-            msg = SCT_Display_Message
-        elseif SCT and SCT.DisplayText then
-            msg = function(message, color)
-                SCT:DisplayMessage(message, color)
-            end
+local function msg(message)
+    UIErrorsFrame:AddMessage(message)
+end
+local color = {r = 1, g = 1, b = 1}
+local ITEM_QUALITY_COLORPATS = {}
+function LootAlert:PLAYER_LOGIN()
+    if MikSBT then
+        msg = function(message)
+            MikSBT.DisplayMessage(message, MikSBT.DISPLAYTYPE_STATIC, false, color.r * 255, color.g * 255, color.b * 255)
         end
-        ChatFrame1:AddMessage("msg is "..tostring(msg))
-    elseif name == 'Blizzard_CombatText' or CombatText_AddMessage then
-        msg = function(message, color)
+    elseif SCT_Display then
+        msg = SCT_Display_Message
+    elseif SCT and SCT.DisplayText then
+        msg = function(message)
+            SCT:DisplayMessage(message, color)
+        end
+    elseif CombatText_AddMessage then
+        msg = function(message)
             CombatText_AddMessage(message, COMBAT_TEXT_SCROLL_FUNCTION, color.r, color.g, color.b, 'sticky', nil)
         end
     end
 
-    if msg then
-        self:RegisterEvent('CHAT_MSG_LOOT')
-        self:RegisterEvent('CHAT_MSG_MONEY')
-        self:UnregisterEvent('ADDON_LOADED')
+    self:RegisterEvent('CHAT_MSG_LOOT')
+    self:RegisterEvent('CHAT_MSG_MONEY')
+
+    for k, v in pairs(ITEM_QUALITY_COLORS) do
+        ITEM_QUALITY_COLORPATS[k] = strformat("|cff%x%x%x", 255 * v.r, 255 * v.g, 255 * v.b)
     end
 end
 
 local solo = YOU_LOOT_MONEY:gsub('%%s', '(.*)')
 local grouped = LOOT_MONEY_SPLIT:gsub('%%s', '(.*)')
-local white = {r = 1, g = 1, b = 1}
 function LootAlert:CHAT_MSG_MONEY(chatmsg)
-    local moneys = chatmsg:match(solo) or chatmsg:match(grouped)
+    local moneys = strmatch(chatmsg, solo) or strmatch(chatmsg, grouped)
     if not moneys then
         return
     end
 
-    local gold   = moneys:match(('(%%d+) %s'):format(GOLD))
-    local silver = moneys:match(('(%%d+) %s'):format(SILVER))
-    local copper = moneys:match(('(%%d+) %s'):format(COPPER))
-    local out = moneymessage:format(moneyformat(gold, silver, copper))
-    msg(out, white)
+    local gold = strmatch(moneys, strformat('(%%d+) %s', GOLD))
+    local silver = strmatch(moneys, strformat('(%%d+) %s', SILVER))
+    local copper = strmatch(moneys, strformat('(%%d+) %s', COPPER))
+    local out = strformat(moneymessage,
+                          strformat('%s%s%s',
+                                    gold and strformat('|cffffd700%sg', gold) or '',
+                                    silver and strformat('|cffc7c7cf%ss', silver) or '',
+                                    copper and strformat('|cffeda55f%sc', copper) or ''))
+
+    msg(out)
 end
 
 local linkpat = '|c........|Hitem:(%%d+):.-|r'
 local single = LOOT_ITEM_SELF:gsub('%%s', linkpat)
 local multiple = LOOT_ITEM_SELF_MULTIPLE:gsub('%%d', '(%%d+)'):gsub('%%s', linkpat)
 function LootAlert:CHAT_MSG_LOOT(chatmsg)
-    local item, count = chatmsg:match(multiple)
+    local item, count = strmatch(chatmsg, multiple)
     if not item then
-        item = chatmsg:match(single)
+        item = strmatch(chatmsg, single)
         count = 1
     end
 
     if item then
         local oldtotal = GetItemCount(item)
         local name, _, rarity = GetItemInfo(item)
-        local color = ITEM_QUALITY_COLORS[rarity]
-        local out = lootmessage:format(name, count, oldtotal + count)
+        local color = ITEM_QUALITY_COLORPATS[rarity]
+        local out = strformat(lootmessage, color, name, count, oldtotal + count)
 
-        msg(out, color)
+        msg(out)
     end
 end
