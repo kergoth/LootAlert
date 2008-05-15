f48afcd519d1b1feddaa7617089e5738540c8b39
Chris Larson <clarson@kergoth.com>
2008-05-15 12:04:46 -0700

    Add deDE translation, courtesy DoofDilla.

---
+++ Locale/deDE.lua
@@ -0,0 +1,34 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("LootAlert", "deDE", false)
+if L then
+    L["Enabled"] = "Aktiviert"
+    L["Enable/Disable the addon."] = "Aktiviere/Deaktiviere das Addon"
+    L["Modify chat messages"] = "Modifiziere die Chat Nachrichten"
+    L["Modify loot chat messages to use LootAlert's formatting."] = "Modifiziere Chat Nachrichten im Format von LootAlert"
+    L["Item Quality Threshold"] = "Item Qualit\195\164ts Grenzwert"
+    L["Threshold"] = "Grenzwert"
+    L["Hide items looted with a lower quality than this."] = "Verstecke gepl\195\188nderte Items mit einer niedrigeren Qualit\195\164t als"
+    L["Apply to chat messages"] = "Auf Chat anwenden"
+    L["Apply item quality threshold to chat messages"] = "Anwenden des Item Qualit\195\164ts Grenzwertes auf den Chat"
+    L["Item Quality Coloring"] = "Item nach Qualitaet faerben"
+    L["Color the item based on its quality, like an item link."] = "F\195\164rbe das Item basierend auf seiner Qualit\195\164t ein"
+    L["Item Icon"] = "Item Icon"
+    L["Text Color"] = "Text Farbe"
+
+    L["Money Format"] = "Geld Format"
+    L["Condensed"] = "Kurzfassung"
+    L["Text"] = "Text"
+    L["Full"] = "Voll"
+
+    L["Example Messages"] = "Beispiel Nachricht"
+    L["Formatting Options"] = "Format Optionen"
+
+    L["Prefix"] = "Vorsilbe"
+    L["Enable a text prefix (i.e. 'Loot: ')"] = "Aktiviere Text Vorsilbe (z.b. 'Loot: ')"
+    L["Prefix Text"] = "Verwende die Vorsilbe"
+
+    L["Output"] = "Ausgabe"
+    L["Loot: "] = "Loot: "
+
+    L["New method"] = "Neue Methode"
+    L["BETA: Use new method of tracking the item counts, to fix the occasional miscount bug."] = "BETA: Benutzte die neue Methode um die Itemzahl zu ermitteln, beseitigt den sporadisch auftretenden Z\195\164hlfehler"
+end
